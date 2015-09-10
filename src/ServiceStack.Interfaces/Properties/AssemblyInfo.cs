0cd3de91c2904f0854c0481f29945e65aca71b7f
voloda <vladimir.kloz@gmail.com>
2015-09-10 13:23:29 +0200

    Make ServiceStack CLS Compliant

--- src/ServiceStack.Interfaces/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Interfaces/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
@@ -27,3 +28,4 @@ using System.Runtime.Serialization;
 	ClrNamespace = "ServiceStack.ServiceInterface.ServiceModel")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
